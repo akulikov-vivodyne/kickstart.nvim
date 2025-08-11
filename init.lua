4cf01c2eff399bd5d6c74d007c6cf0ee10cb8ae3
Anton Kulikov <akulikov@vivodyne.com>
2025-08-10 23:17:21 -0700

    Add prettier.

--- init.lua
+++ init.lua
@@ -967,6 +967,13 @@ require('lazy').setup({
   {
     'github/copilot.vim',
   },
+  {
+    'sbdchd/neoformat',
+    neoformat = {
+      format_on_save = true, -- Enable formatting on save
+      neoformat_try_node_exec = true,
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
